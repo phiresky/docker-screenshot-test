3416dfc30ec79d4982d8db5c0da24038cb347408
phiresky <phireskyde+git@gmail.com>
2020-05-25 14:05:26 +0200

    a

--- integration-tests/main.test.ts
+++ integration-tests/main.test.ts
@@ -1,11 +1,14 @@
-import 'expect-puppeteer'
+import "expect-puppeteer";
+import * as path from "path";
 
-describe('Google', () => {
-  beforeAll(async () => {
-    await page.goto('https://google.com')
-  })
+const screenshotDir = process.env.SCREENSHOT_DIR;
+describe("Google", () => {
+	beforeAll(async () => {
+		await page.goto("https://google.com");
+	});
 
-  it('should display "google" text on page', async () => {
-    await expect(page).toMatch('google')
-  })
-})
+	it('should display "google" text on page', async () => {
+		await expect(page).toMatch("google");
+		if (screenshotDir) await page.screenshot({ path: path.join(screenshotDir, "google-01.png") });
+	});
+});
