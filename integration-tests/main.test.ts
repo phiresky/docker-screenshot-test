bb47bc2c9d065df2dfaa1ff03c6a09d487bf9138
phiresky <phireskyde+git@gmail.com>
2020-05-25 14:07:11 +0200

    a

--- integration-tests/main.test.ts
+++ integration-tests/main.test.ts
@@ -1,7 +1,11 @@
 import "expect-puppeteer";
 import * as path from "path";
-
+import * as fs from "fs";
 const screenshotDir = process.env.SCREENSHOT_DIR;
+if (screenshotDir) {
+	console.log("outputting screenshots to", screenshotDir);
+	fs.mkdirSync(screenshotDir, { recursive: true });
+}
 describe("Google", () => {
 	beforeAll(async () => {
 		await page.goto("https://google.com");
